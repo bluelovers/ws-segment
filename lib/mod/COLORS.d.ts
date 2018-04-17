6412f87908191156391763af6be2233738d61068
bluelovers <codelovers@users.sourceforge.net>
2018-04-17 13:13:58 +0800

    colors

--- lib/mod/COLORS.d.ts
+++ lib/mod/COLORS.d.ts
@@ -1,2 +1,10 @@
-export declare const COLOR_WITH_RGB: string[][];
-export declare const colors: string[];
+import { IDICT } from '../Segment';
+export declare namespace _COLORS {
+    const COLOR_HAIR: string[];
+    const COLOR_WITH_RGB: string[][];
+    const colors: string[];
+    function p(a: string[]): IDICT<number>;
+}
+export declare const COLOR_HAIR: IDICT<number>;
+import * as self from './CHS_NAMES';
+export default self;
