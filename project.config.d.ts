29d26428e4ab5070dde74fac9fbae6f6221790ca
bluelovers <codelovers@users.sourceforge.net>
2018-04-20 23:41:21 +0800

    .

--- project.config.d.ts
+++ project.config.d.ts
@@ -1,6 +1,5 @@
 export declare const project_root: string;
 export declare const dict_root: string;
-export declare const dist_root: string;
 export declare const temp_root: string;
 import * as self from './project.config';
 export default self;
--- project.config.js
+++ project.config.js
@@ -6,7 +6,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const path = require("path");
 exports.project_root = path.join(__dirname);
 exports.dict_root = path.join(exports.project_root, 'dicts');
-exports.dist_root = path.join(exports.project_root, 'dist');
+//export const dist_root = path.join(project_root, 'dist');
 exports.temp_root = path.join(exports.project_root, 'test/temp');
 const self = require("./project.config");
 exports.default = self;
