c58ed0f91f910da6baaf6c3832893a0c10586621
bluelovers <codelovers@users.sourceforge.net>
2018-04-20 23:41:18 +0800

    .

--- project.config.ts
+++ project.config.ts
@@ -8,7 +8,7 @@ export const project_root = path.join(__dirname);
 
 export const dict_root = path.join(project_root, 'dict');
 
-export const dist_root = path.join(project_root, 'dist');
+//export const dist_root = path.join(project_root, 'dist');
 export const temp_root = path.join(project_root, 'test/temp');
 
 import * as self from './project.config';
