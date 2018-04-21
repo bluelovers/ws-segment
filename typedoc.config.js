ba1ccc18d725bf25c36e73552902abf87f6ddbee
bluelovers <codelovers@users.sourceforge.net>
2018-04-21 11:32:06 +0800

    api

--- typedoc.config.js
+++ typedoc.config.js
@@ -4,7 +4,7 @@
 
 const path = require('path');
 
-let p = path.resolve(path.join(path.dirname(require.resolve('typedoc-themes-color'), 'theme')));
+let p = path.resolve(path.join(path.dirname(require.resolve('typedoc-themes-color')), 'theme'));
 
 console.log(p);
 console.log(path.relative(process.cwd(), p));
@@ -13,11 +13,23 @@ module.exports = {
 	src : '.',
 	out: './docs',
 	//theme: './my-theme',
-	theme: path.relative(process.cwd(), p),
-//	theme: p,
+//	theme: path.relative(process.cwd(), p),
+	theme: p,
 	ignoreCompilerErrors: true,
+	excludeExternals: true,
+
+	externalPattern: "**/node_modules/**",
+
 	exclude: [
-		"**/test/*",
+		"test",
+		"node_modules",
+		"test/",
+		"node_modules/",
+		"**/test",
+		"**/node_modules",
+		"**/test/**/*",
 		"**/node_modules/**/*",
 	],
 };
+
+console.log(module.exports);
