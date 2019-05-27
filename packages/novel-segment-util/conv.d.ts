620346c829dd87cc7bd6c91ea77f390d3c218b97
bluelovers <codelovers@users.sourceforge.net>
2019-05-27 19:59:12 +0800

    postpublish new version

--- packages/novel-segment-util/conv.d.ts
+++ packages/novel-segment-util/conv.d.ts
@@ -1,6 +1,6 @@
 /**
  * Created by user on 2019/3/20.
  */
-export declare function getCjkName(w: string, USE_CJK_MODE: number): string;
+export declare function getCjkName(w: string, USE_CJK_MODE?: number): string;
 declare const _default: typeof import("./conv");
 export default _default;
--- packages/novel-segment-util/conv.ts
+++ packages/novel-segment-util/conv.ts
@@ -5,7 +5,7 @@
 import StrUtil = require('str-util');
 import { textList, slugify } from 'cjk-conv/lib/zh/table/list';
 
-export function getCjkName(w: string, USE_CJK_MODE: number)
+export function getCjkName(w: string, USE_CJK_MODE?: number)
 {
 	let cjk_id = slugify(w, true);
 
