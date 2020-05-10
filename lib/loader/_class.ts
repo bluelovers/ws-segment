5e20c6be9ddf5bad606cf95b646026b5c162ba0c
bluelovers <codelovers@users.sourceforge.net>
2020-05-11 01:05:30 +0800

    refactor: split to @novel-segment/stream-loader-core

---
+++ lib/loader/_class.ts
@@ -0,0 +1,8 @@
+/**
+ * Created by user on 2020/5/11.
+ */
+
+export * from '@novel-segment/dict-loader-core';
+import { LoaderClass } from '@novel-segment/dict-loader-core';
+
+export default LoaderClass
