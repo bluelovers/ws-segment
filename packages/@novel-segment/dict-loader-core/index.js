832089eeb4f1ab9e312e63b5f18a216a9456e080
bluelovers <codelovers@users.sourceforge.net>
2020-05-29 16:17:46 +0800

    .

--- packages/@novel-segment/dict-loader-core/index.js
+++ packages/@novel-segment/dict-loader-core/index.js
@@ -18,91 +18,88 @@ const line_1 = require("@novel-segment/stream-loader-core/line");
 const stream_1 = require("@novel-segment/stream-loader-core/stream");
 const sync_1 = require("@novel-segment/stream-loader-core/sync");
 const core_decorators_1 = require("core-decorators");
-let LoaderClass = /** @class */ (() => {
-    let LoaderClass = class LoaderClass {
-        constructor(options = {}, ...argv) {
-            this.default = this.load;
-            if (options.parseLine) {
-                this.parseLine = options.parseLine.bind(this);
-            }
-            if (options.stringifyLine) {
-                this.stringifyLine = options.stringifyLine.bind(this);
-            }
-            if (options.filter) {
-                this.filter = options.filter.bind(this);
-            }
-            if (options.mapper) {
-                this.defaultOptions.mapper = options.mapper.bind(this);
-            }
+let LoaderClass = class LoaderClass {
+    constructor(options = {}, ...argv) {
+        this.default = this.load;
+        if (options.parseLine) {
+            this.parseLine = options.parseLine.bind(this);
         }
-        static create(options = {}, ...argv) {
-            return new this(options, ...argv);
+        if (options.stringifyLine) {
+            this.stringifyLine = options.stringifyLine.bind(this);
         }
-        parseLine(input) {
-            return input;
+        if (options.filter) {
+            this.filter = options.filter.bind(this);
         }
-        stringifyLine(data) {
-            return data.toString();
+        if (options.mapper) {
+            this.defaultOptions.mapper = options.mapper.bind(this);
         }
-        serialize(data) {
-            let self = this;
-            return data.map(function (d) {
-                return self.stringifyLine(d);
-            }).join(crlf_normalize_1.LF);
-        }
-        filter(input) {
-            return input;
-        }
-        load(file, options = {}) {
-            return line_1.wrapStreamToPromise(this.loadStream(file, options))
-                .then(function (stream) {
-                return stream.value;
-            });
-        }
-        loadSync(file, options = {}) {
-            let r = this.loadStreamSync(file, options);
-            let value = r.value;
-            // 試圖手動清除記憶體占用
-            r = undefined;
-            return value;
-        }
-        loadStream(file, options = {}, callback) {
-            return this._createStream(stream_1.default, file, options, callback);
-        }
-        loadStreamSync(file, options = {}, callback) {
-            return this._createStream(sync_1.default, file, options, callback);
-        }
-        _createStream(fnStream, file, options = {}, callback) {
-            let self = this;
-            let opts = Object.assign({}, this.defaultOptions, options);
-            let parseLine = opts.parseLine || self.parseLine;
-            let filter = opts.filter || self.filter;
-            opts.parseLine = parseLine;
-            let stream = fnStream(file, {
-                callback,
-                mapper: opts.mapper || function mapper(line) {
-                    if (filter) {
-                        line = filter(line);
-                    }
-                    if (line) {
-                        // @ts-ignore
-                        return parseLine(line, self.parseLine);
-                    }
-                },
-            });
-            // @ts-ignore
-            stream.pipeLoader = self;
-            // @ts-ignore
-            stream.pipeRuntimeOptions = opts;
-            return stream;
-        }
-    };
-    LoaderClass = __decorate([
-        core_decorators_1.autobind,
-        __metadata("design:paramtypes", [Object, Object])
-    ], LoaderClass);
-    return LoaderClass;
-})();
+    }
+    static create(options = {}, ...argv) {
+        return new this(options, ...argv);
+    }
+    parseLine(input) {
+        return input;
+    }
+    stringifyLine(data) {
+        return data.toString();
+    }
+    serialize(data) {
+        let self = this;
+        return data.map(function (d) {
+            return self.stringifyLine(d);
+        }).join(crlf_normalize_1.LF);
+    }
+    filter(input) {
+        return input;
+    }
+    load(file, options = {}) {
+        return line_1.wrapStreamToPromise(this.loadStream(file, options))
+            .then(function (stream) {
+            return stream.value;
+        });
+    }
+    loadSync(file, options = {}) {
+        let r = this.loadStreamSync(file, options);
+        let value = r.value;
+        // 試圖手動清除記憶體占用
+        r = undefined;
+        return value;
+    }
+    loadStream(file, options = {}, callback) {
+        return this._createStream(stream_1.default, file, options, callback);
+    }
+    loadStreamSync(file, options = {}, callback) {
+        return this._createStream(sync_1.default, file, options, callback);
+    }
+    _createStream(fnStream, file, options = {}, callback) {
+        let self = this;
+        let opts = Object.assign({}, this.defaultOptions, options);
+        let parseLine = opts.parseLine || self.parseLine;
+        let filter = opts.filter || self.filter;
+        opts.parseLine = parseLine;
+        let stream = fnStream(file, {
+            callback,
+            mapper: opts.mapper || function mapper(line) {
+                if (filter) {
+                    line = filter(line);
+                }
+                if (line) {
+                    // @ts-ignore
+                    return parseLine(line, self.parseLine);
+                }
+            },
+        });
+        // @ts-ignore
+        stream.pipeLoader = self;
+        // @ts-ignore
+        stream.pipeRuntimeOptions = opts;
+        return stream;
+    }
+};
+LoaderClass = __decorate([
+    core_decorators_1.autobind,
+    __metadata("design:paramtypes", [Object, Object])
+], LoaderClass);
 exports.LoaderClass = LoaderClass;
 exports.default = LoaderClass;
 //# sourceMappingURL=index.js.map
