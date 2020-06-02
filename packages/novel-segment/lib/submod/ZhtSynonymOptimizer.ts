bf4aa7cdbc24b513889175312c6218783b392029
bluelovers <codelovers@users.sourceforge.net>
2020-06-03 02:27:25 +0800

    feat: 優化 修正 几百遍 => 幾百遍

--- packages/novel-segment/lib/submod/ZhtSynonymOptimizer.ts
+++ packages/novel-segment/lib/submod/ZhtSynonymOptimizer.ts
@@ -506,11 +506,27 @@ export class ZhtSynonymOptimizer extends SubSModuleOptimizer
 				}
 				// 如果項目為 量词
 				else if (hexAndAny(w1.p,
-					POSTAG.A_Q,
+					//POSTAG.A_Q,
 					POSTAG.D_MQ,
 				))
 				{
+					if (/^几/.test(w1.w) && w1.m?.length > 1)
+					{
+						/*
+						let m = w1.m as IWord[];
+						if (m[0].p & POSTAG.D_MQ)
+						{
 
+						}
+						 */
+
+						let nw = w1.w.replace(/^几/, '幾');
+
+						w1.ow = w1.w;
+						w1.w = nw;
+
+						bool = true;
+					}
 				}
 				else if (w1.p & POSTAG.D_V && /^干(.)$/.test(w1.w))
 				{
