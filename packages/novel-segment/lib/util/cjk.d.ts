d8b0efac26d3c00b5f4372d0aa82c59cc3599974
bluelovers <codelovers@users.sourceforge.net>
2020-06-07 04:13:17 +0800

    feat: use @lazy-cjk/zh-table-list

--- packages/novel-segment/lib/util/cjk.d.ts
+++ packages/novel-segment/lib/util/cjk.d.ts
@@ -1,5 +1,5 @@
 /**
  * Created by user on 2018/4/15/015.
  */
-import { charTableList as char_table, textList as text_list, arrCjk as arr_cjk } from 'cjk-conv/lib/zh/table/list';
+import { charTableList as char_table, textList as text_list, arrCjk as arr_cjk } from '@lazy-cjk/zh-table-list/list';
 export { char_table, text_list, arr_cjk };
--- packages/novel-segment/lib/util/cjk.js
+++ packages/novel-segment/lib/util/cjk.js
@@ -4,7 +4,7 @@
  */
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.arr_cjk = exports.text_list = exports.char_table = void 0;
-const list_1 = require("cjk-conv/lib/zh/table/list");
+const list_1 = require("@lazy-cjk/zh-table-list/list");
 Object.defineProperty(exports, "char_table", { enumerable: true, get: function () { return list_1.charTableList; } });
 Object.defineProperty(exports, "text_list", { enumerable: true, get: function () { return list_1.textList; } });
 Object.defineProperty(exports, "arr_cjk", { enumerable: true, get: function () { return list_1.arrCjk; } });
