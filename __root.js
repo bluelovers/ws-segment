d002b78b2234d2ada65883cca4edc60e71e4afbf
bluelovers <codelovers@users.sourceforge.net>
2020-07-01 15:31:06 +0800

    chore: add __root

---
+++ __root.js
@@ -0,0 +1,7 @@
+"use strict";
+/**
+ * Created by user on 2020/7/1.
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = __dirname;
+//# sourceMappingURL=__root.js.map
---
+++ __root.ts
@@ -0,0 +1,7 @@
+/**
+ * Created by user on 2020/7/1.
+ */
+
+import { join, normalize } from "path";
+
+export default __dirname;
