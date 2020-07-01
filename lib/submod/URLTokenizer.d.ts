7e41c6eeda921a51635df1ab8974e8b1a12e94c7
bluelovers <codelovers@users.sourceforge.net>
2020-07-01 15:45:57 +0800

    refactor: update URLTokenizer

--- lib/submod/URLTokenizer.d.ts
+++ lib/submod/URLTokenizer.d.ts
@@ -1,16 +1,27 @@
+import Segment, { IWord } from '../Segment';
 /**
  * URL识别模块
  *
  * @author 老雷<leizongmin@gmail.com>
  */
-declare var debug: {
-    (...data: any[]): void;
-    (message?: any, ...optionalParams: any[]): void;
-};
-declare var PROTOTAL: string[];
-declare var MIN_PROTOTAL_LEN: number;
-declare var _URLCHAR: string[];
-declare var URLCHAR: {};
+/**
+ * 模块类型
+ * */
+export declare const type = "tokenizer";
+export declare let segment: Segment;
+/**
+ * 模块初始化
+ *
+ * @param {Segment} segment 分词接口
+ */
+export declare function init(_segment: Segment): void;
+/**
+ * 对未识别的单词进行分词
+ *
+ * @param {array} words 单词数组
+ * @return {array}
+ */
+export declare function split(words: IWord[]): IWord[];
 /**
  * 匹配包含的网址，返回相关信息
  *
@@ -18,4 +29,4 @@ declare var URLCHAR: {};
  * @param {int} cur 开始位置
  * @return {array}  返回格式   {w: '网址', c: 开始位置}
  */
-declare var matchURL: (text: any, cur: any) => any[];
+export declare function matchURL(text: string, cur?: number): any[];
