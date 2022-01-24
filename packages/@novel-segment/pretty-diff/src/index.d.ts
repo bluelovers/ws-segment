b04ed5ca79a2de9b891fcc3c7f2a4c601cd5eaa6
bluelovers <codelovers@users.sourceforge.net>
2022-01-24 08:11:08 +0800

    .

---
+++ packages/@novel-segment/pretty-diff/src/index.d.ts
@@ -0,0 +1,11 @@
+import { ITSValueOrArrayMaybeReadonly } from 'ts-type/lib/type/base';
+import { IWord } from '@novel-segment/types';
+export declare type ITextInput = ITSValueOrArrayMaybeReadonly<IWord | string>;
+export declare function printPrettyDiff(text_old: ITextInput, text_new: ITextInput): {
+    text_old: string;
+    text_new: string;
+    changed: boolean;
+    text_new2: string;
+};
+export declare function diff_log(src_text: string, new_text: string): string;
+export default printPrettyDiff;
