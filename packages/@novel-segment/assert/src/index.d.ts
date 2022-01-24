97e9cff4c97fea4a7497ba01e5309b8e5c06f650
bluelovers <codelovers@users.sourceforge.net>
2022-01-24 09:56:16 +0800

    chore: update jsdoc

--- packages/@novel-segment/assert/src/index.d.ts
+++ packages/@novel-segment/assert/src/index.d.ts
@@ -1,13 +1,81 @@
 export interface IOptionsLazyMatch {
     firstOne?: boolean;
+    /**
+     * @example
+     * inspectFn = chai.util.inspect
+     * @example
+     * import { inspect } from 'util';
+     * inspectFn = inspect
+     */
     inspectFn?(input: any, ...argv: any[]): any;
 }
 export declare function _handleLazyMatchOptions(options?: IOptionsLazyMatch): {
     inspectFn: (input: any, ...argv: any[]) => any;
     firstOne?: boolean;
 };
+/**
+ * 分析後應該要符合以下結果
+ * @example
+ * [
+ * 		'胡锦涛出席APEC领导人会议后回京',
+ * 		[
+ * 			'会议',
+ * 			'回京',
+ * 		],
+ * 	],
+ */
 export declare function lazyMatch(a: string[], b: string[] | (string | string[])[], options?: IOptionsLazyMatch): boolean;
+/**
+ * 分析後應該要符合以下其中一個結果
+ * @example
+ * [
+ * 		'在這裡有兩具自動人偶隨侍在側的烏列爾',
+ * 		[
+ * 			[
+ * 				'兩具',
+ * 				'自動',
+ * 				'人偶',
+ * 				'隨侍',
+ * 			],
+ * 			[
+ * 				'兩具',
+ * 				'自動人偶',
+ * 				'隨侍',
+ * 			],
+ * 		],
+ * 	],
+ */
 export declare function lazyMatch002(a: string[], b_arr: Parameters<typeof lazyMatch>['1'][], options?: IOptionsLazyMatch): void;
+/**
+ * 分析轉換後應該要具有以下字詞
+ * @example
+ * [
+ * 		'大家干的好',
+ * 		[
+ * 			'幹',
+ * 		],
+ * 	],
+ */
 export declare function lazyMatchSynonym001(a: string, b_arr: (string | string[])[], options?: IOptionsLazyMatch): void;
+/**
+ * 分析轉換後不應該具有以下字詞
+ * @example
+ * [
+ * 		'那是里靈魂的世界。',
+ * 		[
+ * 			'裡',
+ * 		],
+ * 	],
+ */
 export declare function lazyMatchSynonym001Not(a: string, b_arr: (string | string[])[], options?: IOptionsLazyMatch): void;
+/**
+ * 分析後不應該存在符合以下結果
+ * @example
+ * [
+ * 		'這份毫不守舊的率直',
+ * 		[
+ * 			'份毫',
+ * 		],
+ * 	],
+ */
 export declare function lazyMatchNot(a: string[], b: string[] | (string | string[])[], options?: IOptionsLazyMatch): boolean;
--- packages/@novel-segment/assert/src/index.ts
+++ packages/@novel-segment/assert/src/index.ts
@@ -4,6 +4,14 @@ import { fail } from 'assert';
 export interface IOptionsLazyMatch
 {
 	firstOne?: boolean,
+
+	/**
+	 * @example
+	 * inspectFn = chai.util.inspect
+	 * @example
+	 * import { inspect } from 'util';
+	 * inspectFn = inspect
+	 */
 	inspectFn?(input: any, ...argv: any[]): any,
 }
 
@@ -16,6 +24,17 @@ export function _handleLazyMatchOptions(options: IOptionsLazyMatch = {})
 	}
 }
 
+/**
+ * 分析後應該要符合以下結果
+ * @example
+ * [
+ * 		'胡锦涛出席APEC领导人会议后回京',
+ * 		[
+ * 			'会议',
+ * 			'回京',
+ * 		],
+ * 	],
+ */
 export function lazyMatch(a: string[], b: string[] | (string | string[])[], options: IOptionsLazyMatch = {})
 {
 	let i: number = null;
@@ -91,6 +110,26 @@ export function lazyMatch(a: string[], b: string[] | (string | string[])[], opti
 	return bool;
 }
 
+/**
+ * 分析後應該要符合以下其中一個結果
+ * @example
+ * [
+ * 		'在這裡有兩具自動人偶隨侍在側的烏列爾',
+ * 		[
+ * 			[
+ * 				'兩具',
+ * 				'自動',
+ * 				'人偶',
+ * 				'隨侍',
+ * 			],
+ * 			[
+ * 				'兩具',
+ * 				'自動人偶',
+ * 				'隨侍',
+ * 			],
+ * 		],
+ * 	],
+ */
 export function lazyMatch002(a: string[], b_arr: Parameters<typeof lazyMatch>['1'][], options: IOptionsLazyMatch = {})
 {
 	let bool: boolean;
@@ -117,6 +156,16 @@ export function lazyMatch002(a: string[], b_arr: Parameters<typeof lazyMatch>['1
 	!bool && fail(`expected ${options.inspectFn(a)} to have includes one of ordered members in ${options.inspectFn(b_arr)}`);
 }
 
+/**
+ * 分析轉換後應該要具有以下字詞
+ * @example
+ * [
+ * 		'大家干的好',
+ * 		[
+ * 			'幹',
+ * 		],
+ * 	],
+ */
 export function lazyMatchSynonym001(a: string, b_arr: (string | string[])[], options: IOptionsLazyMatch = {})
 {
 	let bool: boolean;
@@ -176,6 +225,16 @@ export function lazyMatchSynonym001(a: string, b_arr: (string | string[])[], opt
 	!bool && fail(`expected ${inspectFn(a)} to have index of ordered members in ${inspectFn(b_arr)}`);
 }
 
+/**
+ * 分析轉換後不應該具有以下字詞
+ * @example
+ * [
+ * 		'那是里靈魂的世界。',
+ * 		[
+ * 			'裡',
+ * 		],
+ * 	],
+ */
 export function lazyMatchSynonym001Not(a: string, b_arr: (string | string[])[], options: IOptionsLazyMatch = {})
 {
 	let bool: boolean;
@@ -228,6 +287,16 @@ export function lazyMatchSynonym001Not(a: string, b_arr: (string | string[])[], 
 	});
 }
 
+/**
+ * 分析後不應該存在符合以下結果
+ * @example
+ * [
+ * 		'這份毫不守舊的率直',
+ * 		[
+ * 			'份毫',
+ * 		],
+ * 	],
+ */
 export function lazyMatchNot(a: string[], b: string[] | (string | string[])[], options: IOptionsLazyMatch = {})
 {
 	let i: number = null;
