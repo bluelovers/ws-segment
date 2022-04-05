754e0a0588458175d9a4f9faa31f21710bff9249
bluelovers <codelovers@users.sourceforge.net>
2022-04-05 10:51:19 +0800

    .

--- packages/@novel-segment/stringify/dist/index.cjs.production.min.cjs
+++ packages/@novel-segment/stringify/dist/index.cjs.production.min.cjs
@@ -1,2 +1,2 @@
-"use strict";function t(t,...e){return t.map((function(t){if("string"==typeof t)return t;if("w"in t)return t.w;throw new TypeError("not a valid segment result list")}))}function e(e,...r){return t(e,...r).join("")}Object.defineProperty(exports,"__esModule",{value:!0}),exports.default=e,exports.stringify=e,exports.stringifyList=t;
+"use strict";function stringifyList(t,...i){return t.map((function(t){if("string"==typeof t)return t;if("w"in t)return t.w;throw new TypeError("not a valid segment result list")}))}function stringify(t,...i){return stringifyList(t,...i).join("")}Object.defineProperty(exports,"__esModule",{value:!0}),exports.default=stringify,exports.stringify=stringify,exports.stringifyList=stringifyList;
 //# sourceMappingURL=index.cjs.production.min.cjs.map
