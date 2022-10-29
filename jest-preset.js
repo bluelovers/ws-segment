cbce9a2868e5a0a95fd8f026530c34c9f3930ba0
bluelovers <codelovers@users.sourceforge.net>
2022-10-30 07:12:55 +0800

    test: update jest config

--- jest-preset.js
+++ jest-preset.js
@@ -1,5 +1,15 @@
 const { mixinJestConfig } = require('@bluelovers/jest-config');
+const { defaults } = require('jest-config');
 
-module.exports = mixinJestConfig({}, true, {
+/**
+ * // @type { import('@jest/types').Config.InitialOptions }
+ * // @type { import('ts-jest').InitialOptionsTsJest }
+ * @type { import('ts-jest').JestConfigWithTsJest }
+ */
+const jestConfig = mixinJestConfig({}, true, {
 	file: __filename,
-})
+});
+
+jestConfig.testRegex = ['\\.spec\\.(ts|tsx)$'];
+
+module.exports = jestConfig;
