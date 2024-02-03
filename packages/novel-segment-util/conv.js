8da1fc857e9ca7beb3c0becf4128ed5dac48a2be
bluelovers <codelovers@users.sourceforge.net>
2024-02-04 03:01:41 +0800

    .

--- packages/novel-segment-util/conv.js
+++ packages/novel-segment-util/conv.js
@@ -4,11 +4,11 @@
  */
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.getCjkName = void 0;
-const str_util_1 = require("str-util");
+const fullhalf_1 = require("@lazy-cjk/fullhalf");
 const zh_slugify_1 = require("@lazy-cjk/zh-slugify");
 function getCjkName(w, USE_CJK_MODE) {
     let cjk_id = (0, zh_slugify_1.slugify)(w, true);
-    return (0, str_util_1.toHalfWidth)(cjk_id).toLocaleLowerCase();
+    return (0, fullhalf_1.toHalfWidth)(cjk_id).toLocaleLowerCase();
 }
 exports.getCjkName = getCjkName;
 exports.default = getCjkName;
--- packages/novel-segment-util/conv.ts
+++ packages/novel-segment-util/conv.ts
@@ -2,7 +2,7 @@
  * Created by user on 2019/3/20.
  */
 
-import { toHalfWidth } from 'str-util';
+import { toHalfWidth } from '@lazy-cjk/fullhalf';
 import { slugify } from '@lazy-cjk/zh-slugify';
 
 export function getCjkName(w: string, USE_CJK_MODE?: number)
